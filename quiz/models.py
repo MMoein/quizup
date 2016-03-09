9c23734af4eafa9aea85eaed35f29e038360a7fc
shervin <shervin.h.esmaili@gmail.com>
2016-03-09 06:11:09 +0330

    added answers to questions.

--- quiz/models.py
+++ quiz/models.py
@@ -11,3 +11,7 @@ class QuestionCategory(models.Model):
 class Question(models.Model):
     text = models.TextField(verbose_name="Question")
     category = models.ForeignKey(QuestionCategory, verbose_name="Category")
+    choice1 = models.CharField(max_length=255, verbose_name="Right answer")
+    choice2 = models.CharField(max_length=255, verbose_name="Dummy answer1")
+    choice3 = models.CharField(max_length=255, verbose_name="Dummy answer2")
+    choice4 = models.CharField(max_length=255, verbose_name="Dummy answer3")
