eac5b5506ff3ef854876b75b9a0eaaa662d9b133
mahan94 <azm.mahan@gmail.com>
2016-04-06 07:31:43 +0430

    edit profile

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
+
 import socket
 
 from django.contrib.auth import authenticate
 from django.core.urlresolvers import reverse
 from django.shortcuts import render
 from .forms import *
-
 
 import sha, random
 from django.http import HttpResponse
@@ -16,8 +17,6 @@ from django.core.mail import send_mail
 from django.contrib.auth import login as auth_login
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
-
-
 
 
 def signup(request):
@@ -33,7 +32,7 @@ def signup(request):
             user = User(**user_form.cleaned_data)
             user.username = user.email
             user.set_password(user.password)
-            if User.objects.filter(email = user.email):
+            if User.objects.filter(email=user.email):
                 errors = "already have this"
             else:
                 user.save()
@@ -41,35 +40,37 @@ def signup(request):
                 profile.user = user
                 profile.is_active = False
                 salt = sha.new(str(random.random())).hexdigest()[:5]
-                activation_key = sha.new(salt+user.username).hexdigest()
+                activation_key = sha.new(salt + user.username).hexdigest()
                 profile.activation_key = activation_key
                 # Now we save the UserProfile model instance.
                 profile.save()
                 s = socket.gethostbyname(socket.gethostname())
-                activation_url = "http://"+str(s)+"/verify/" + activation_key
-                send_mail('Activation link', activation_url , 'moein.zeraatkar@gmail.comf',
-                        [user.email], fail_silently=False)
+                activation_url = "http://" + str(s) + "/verify/" + activation_key
+                send_mail('Activation link', activation_url, 'moein.zeraatkar@gmail.comf',
+                          [user.email], fail_silently=False)
                 registered = True
         else:
             errors = str(user_form.errors) + str(profile_form.errors)
-        return redirect(reverse('login'))
+            # return redirect(reverse('login'))
     elif request.method == 'GET':
         user_form = UserForm()
         profile_form = UserProfileForm()
-        return render_to_response('Authentication/signup.html',
-                                {'user_form':user_form, 'profile_form':profile_form, 'registered':registered, 'error_message':errors},
-                                context_instance=RequestContext(request))
+
+    return render_to_response('Authentication/signup.html',
+                              {'user_form': user_form, 'profile_form': profile_form, 'registered': registered,
+                               'error_message': errors},
+                              context_instance=RequestContext(request))
 
 
 def verify(request, token):
     userprofilelist = UserProfile.objects.filter(
-        activation_key = token)
+        activation_key=token)
     if not userprofilelist:
         return HttpResponse("Error")
     userprofile = userprofilelist[0]
     userprofile.is_active = True
     userprofile.save()
-    auth_login(request,userprofile)
+    auth_login(request, userprofile)
     return HttpResponse("user " + str(userprofile) + " activated")
 
 
@@ -84,9 +85,9 @@ def login(request):
             password = loginform.cleaned_data['password']
             user = authenticate(username=username, password=password)
             if user is not None:
-                up = get_object_or_404(UserProfile, user = user)
+                up = get_object_or_404(UserProfile, user=user)
                 if up.is_active is True:
-                    auth_login(request,user)
+                    auth_login(request, user)
                     return redirect('/')
                 else:
                     return HttpResponse("username is not active ! check your email plz!")
@@ -97,3 +98,74 @@ def login(request):
     return render_to_response('Authentication/login.html',
                               {'form': LoginForm(), 'error_message': errors},
                               context_instance=RequestContext(request))
+
+
+def profile(request):
+    print "profile"
+    if not request.user.is_authenticated():
+        print "redirected"
+        return redirect(reverse('login'))
+
+    errors = []
+    if request.method == 'POST':
+
+        user_form = UserForm(data=request.POST, instance=request.user)
+
+        user_profile = UserProfile.objects.get(user=request.user)
+        profile_form = UserProfileForm(data=request.POST, instance=user_profile)
+
+        # If the two forms are valid...
+        if user_form.is_valid() and profile_form.is_valid():
+
+            user_form.save()
+            # user = User(**user_form.cleaned_data)
+            # user.first_name = user.first_name
+            # user.last_name = user.last_name
+            # user.username = user.email
+            # user.set_password(user.password)
+
+            # profile = profile_form.save(commit=False)
+            # profile.user = user
+
+
+            # if User.objects.filter(email=user.email):
+            #     errors = "already have this"
+            # else:
+            #     user.save()
+            #     profile = profile_form.save(commit=False)
+            #     profile.user = user
+            #     profile.is_active = False
+            #     salt = sha.new(str(random.random())).hexdigest()[:5]
+            #     activation_key = sha.new(salt + user.username).hexdigest()
+            #     profile.activation_key = activation_key
+            #     # Now we save the UserProfile model instance.
+            #     profile.save()
+            #     s = socket.gethostbyname(socket.gethostname())
+            #     activation_url = "http://" + str(s) + "/verify/" + activation_key
+            #     send_mail('Activation link', activation_url, 'moein.zeraatkar@gmail.comf',
+            #               [user.email], fail_silently=False)
+            #     registered = True
+            return render_to_response('Authentication/profile.html',
+                                      {'user_form': user_form,
+                                       'profile_form': profile_form,
+                                       'messages': u'تغییرات اعمال شد',
+                                       'error_message': errors},
+                                      context_instance=RequestContext(request))
+        else:
+            errors = str(user_form.errors) + str(profile_form.errors)
+            # return redirect(reverse('profile))
+            return render_to_response('Authentication/profile.html',
+                                      {'user_form': user_form,
+                                       'profile_form': profile_form,
+                                       'error_message': errors},
+                                      context_instance=RequestContext(request))
+
+    elif request.method == 'GET':
+        user_profile = UserProfile.objects.get(user=request.user)
+        user_form = UserForm(instance=request.user)
+        user_profile_form = UserProfileForm(instance=user_profile)
+        return render_to_response('Authentication/profile.html',
+                                  {'user_form': user_form,
+                                   'profile_form': user_profile_form,
+                                   'error_message': errors},
+                                  context_instance=RequestContext(request))
